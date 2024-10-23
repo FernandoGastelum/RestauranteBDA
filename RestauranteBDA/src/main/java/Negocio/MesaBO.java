fb3ebc426105bd75e38ba15e02ce25b81274bca4
Oribiel <153157241+Oribiel@users.noreply.github.com>
2024-10-23 13:59:45 -0600

    correcciones bo
    1

--- RestauranteBDA/src/main/java/Negocio/MesaBO.java
+++ RestauranteBDA/src/main/java/Negocio/MesaBO.java
@@ -7,16 +7,17 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import DAO.MesaDAO;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MesaBO {
 
     private MesaDAO mesaDAO;
 
     public MesaBO() {
-        this.mesaDAO = new MesaDAO();  // Instancia DAO para manejar las operaciones de la base de datos
+        this.mesaDAO = new MesaDAO();
     }
 
-    public void registrarMesa(MesaDTO mesaDTO) throws Exception {
+    public void registrarMesas(int cantidadPequenas, int cantidadMedianas, int cantidadGrandes, String ubicacion) throws Exception {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu_restaurante");
         EntityManager em = emf.createEntityManager();
@@ -25,22 +26,40 @@ public class MesaBO {
 
             em.getTransaction().begin();
 
-            Mesa mesa = new Mesa(mesaDTO.getTipo(), mesaDTO.getCapacidad(), mesaDTO.getUbicacion(), mesaDTO.getCodigo(), new ArrayList<>());
+            generarMesas(em, cantidadPequenas, 2, "Pequeña", ubicacion);
 
-            mesaDAO.registrarMesa(mesa);
+            generarMesas(em, cantidadMedianas, 4, "Mediana", ubicacion);
+
+            generarMesas(em, cantidadGrandes, 8, "Grande", ubicacion);
 
             em.getTransaction().commit();
-            System.out.println("Mesa registrada con éxito.");
+            System.out.println("Mesas registradas con éxito.");
 
         } catch (Exception e) {
-
             em.getTransaction().rollback();
             throw e;
-
         } finally {
-
             em.close();
             emf.close();
         }
+    }
+
+    private void generarMesas(EntityManager em, int cantidad, int capacidad, String tipo, String ubicacion) throws Exception {
+        for (int i = 1; i <= cantidad; i++) {
+
+            String codigo = generarCodigoMesa(ubicacion, capacidad, i);
+
+            Mesa mesa = new Mesa(tipo, capacidad, ubicacion, codigo, new ArrayList<>());
+            mesaDAO.registrarMesa(mesa);
+        }
+    }
+
+    private String generarCodigoMesa(String ubicacion, int capacidad, int numero) {
+
+        String codigoUbicacion = ubicacion.substring(0, 3).toUpperCase();
+
+        String numeroMesa = String.format("%03d", numero);
+
+        return codigoUbicacion + "-" + capacidad + "-" + numeroMesa;
     }
 }

bea8af5544eb9c5db95341632226a8bd2df6bfe7
FernandoGastelum <ilian.gastelum228761@potros.itson.edu.mx>
2024-10-22 19:40:28 -0700

    Proyecto base

---
+++ RestauranteBDA/src/main/java/mx/itson/restaurantebda/RestauranteBDA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.itson.restaurantebda;
+
+/**
+ *
+ * @author gaspa
+ */
+public class RestauranteBDA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
